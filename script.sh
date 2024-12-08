82385b850f996ebfd90cebf8fc00e4f12f480a07
SankalpKawade <kawadesankalp00@gmail.com>
2024-12-08 06:09:07 +0000

    updates script

--- script.sh
+++ script.sh
@@ -6,10 +6,5 @@ sudo apt update
 echo "Deploying webApp on Nginx"
 sudo apt install nginx -y
 
-echo "remove file from document root"
-rm -r /var/www/html
-
-echo "clone directory"
-sudo git clone https://github.com/SankalpKawade/devops-project.git /var/www/html
 
 
