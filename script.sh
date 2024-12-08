916bb592bc82749f21ce7890227eec75d42e005f
SankalpKawade <kawadesankalp00@gmail.com>
2024-12-08 05:27:08 +0000

    updates script for deployment

--- script.sh
+++ script.sh
@@ -1,2 +1,15 @@
 #!/bin/bash
+
+echo "update server"
+sudo apt update
+
 echo "Deploying webApp on Nginx"
+sudo apt install nginx -y
+
+echo "remove file from document root"
+rm -r /var/www/html
+
+echo "clone directory"
+sudo git clone https://github.com/SankalpKawade/devops-project.git /var/www/html
+
+
